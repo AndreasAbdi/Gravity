383605ef1f5c51ef574305466181dadb8bf790be
AndreasAbdi <andreasabdi@yahoo.ca>
2016-08-25 15:39:51 -0700

    Add Boost libraries to build
    Add the regex library and basic stuff to the cmakelists.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,15 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
+#include <boost/regex.hpp>
+#include <string>
 
 int main() {
-    printf("Gravity \n");
+	std::string line = "Subject: Re: GRAVITY";
+	boost::regex pattern("^Subject: (Re: |Aw: )*(.*)$");
+	boost::smatch matches;
+	if (boost::regex_match(line, matches, pattern)) {
+		std::cout << matches[2] << std::endl;
+	}
     return 0;
 }
