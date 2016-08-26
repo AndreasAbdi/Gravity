e58ae81f9e04a23ea4c03ad9c0eec77356eddbd1
AndreasAbdi <andreasabdi@yahoo.ca>
2016-08-25 17:19:43 -0700

    Add SDL2
    Add the library and the headers for SDL2. Added some test code to the main file.

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,19 @@
 #include <iostream>
 #include <algorithm>
 #include <boost/regex.hpp>
+#include "SDL.h"
 #include <string>
-
-int main() {
+ 
+int main(int argc, char* args[]) {
 	std::string line = "Subject: Re: GRAVITY";
 	boost::regex pattern("^Subject: (Re: |Aw: )*(.*)$");
 	boost::smatch matches;
 	if (boost::regex_match(line, matches, pattern)) {
 		std::cout << matches[2] << std::endl;
 	}
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Quit();
     return 0;
 }
